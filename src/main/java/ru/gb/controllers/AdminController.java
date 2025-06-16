bdf4688d668cff3987698161b55038d715e3d9df
abdugafforkabiljanov <tursunboyevabarno1980@gmail.com>
2025-06-16 17:50:13 +0500

    File upload system successfully implemented

--- src/main/java/ru/gb/controllers/AdminController.java
+++ src/main/java/ru/gb/controllers/AdminController.java
@@ -1,22 +1,47 @@
 package ru.gb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import ru.gb.service.ImageService;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private ImageService imageService;
+
+
     @GetMapping
     public String showAdminPage(Model model) {
         model.addAttribute("title", "Admin Paneli");
         return "admin";
     }
-        @GetMapping("/add-product")
-        public String showAddProductPage (Model model){
-            model.addAttribute("title", "Mahsulot Qo‘shish");
-            return "add-product";
+    @GetMapping("/add-product")
+    public String showAddProductPage (Model model){
+        model.addAttribute("title", "Mahsulot Qo‘shish");
+        return "add-product";
+    }
+
+
+    @PostMapping("/add-product")
+    public String addProduct(@RequestParam("image")MultipartFile image , Model model) throws IOException {
+        if (imageService.uploadImage(image)) {
+            model.addAttribute("message", "Rasm muvaffaqiyatli yuklandi!");
+        } else {
+            model.addAttribute("message", "Rasm yuklashda xatolik yuz berdi!");
         }
+        return "redirect:/admin/add-product";
+
+    }
+
 
 }
---
+++ src/main/java/ru/gb/service/ImageService.java
@@ -0,0 +1,61 @@
+package ru.gb.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ImageService {
+
+    @Value("${image.upload.dir}")
+    private String imageUploadDir;
+
+    private final List<String> allowedFormats = Arrays.asList("png", "jpg", "jpeg", "tiff", "tif", "webp", "heif", "heic");
+    private final long maxFileSize = 5 * 1024 * 1024;
+
+    public boolean uploadImage(MultipartFile image) {
+        try {
+            if (image == null || image.isEmpty()) {
+                return false;
+            }
+
+            String originalImageName = image.getOriginalFilename();
+            if (originalImageName == null) {
+                return false;
+            }
+
+            int lastDotIndex = originalImageName.lastIndexOf(".");
+            if (lastDotIndex == -1) {
+                return false;
+            }
+
+            String extension = originalImageName.substring(lastDotIndex + 1).toLowerCase();
+            if (!allowedFormats.contains(extension)) {
+                return false;
+            }
+
+            if (image.getSize() > maxFileSize) {
+                return false;
+            }
+
+
+            File uploadDir = new File(imageUploadDir);
+
+            String fileName = UUID.randomUUID().toString() + "_" + originalImageName + System.currentTimeMillis();
+            File destFile = new File(uploadDir, fileName);
+
+            image.transferTo(destFile);
+            return true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
